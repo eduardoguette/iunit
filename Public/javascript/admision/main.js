fecb3b2b8e58d50b8e7d567b7031796252285039
Eduardo Guette <eduardoguette@github.com>
2020-07-01 19:19:31 +0200

    javascript add

---
+++ Public/javascript/admision/main.js
@@ -0,0 +1,26 @@
+ const down_angle = document.querySelectorAll('div.prsri-btn i.fas.fa-angle-up').forEach(a =>{
+  a.classList.add('angle-inactive')
+})  
+const info = document.querySelectorAll('.modal-info').forEach(a =>{
+  a.classList.add('angle-inactive')
+})  
+
+document.querySelector('.prsri').addEventListener("click", function(e){
+  e.preventDefault()
+  console.log(e.target.className)
+  let btns = e.path[2];
+  let container = e.path[4]
+  if(e.target.className == 'fas fa-angle-down'){
+    btns.querySelector('.fas.fa-angle-up').classList.remove('angle-inactive')
+    btns.querySelector('.fas.fa-angle-down').classList.add('angle-inactive')
+    /* div info */
+    container.querySelector('.modal-info').classList.remove('angle-inactive')
+    console.log(container)
+  }else{
+    btns.querySelector('.fas.fa-angle-down').classList.remove('angle-inactive')
+    btns.querySelector('.fas.fa-angle-up').classList.add('angle-inactive')
+      /* div info */
+    container.querySelector('.modal-info').classList.add('angle-inactive')
+    console.log(e)
+  }
+})
