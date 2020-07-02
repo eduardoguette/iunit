4cd66294a4f05df95b0bfb9acb07114c783a8fc9
Eduardo Guette <eduardoguette@github.com>
2020-07-02 22:09:26 +0200

    togleee done

--- Public/javascript/admision/main.js
+++ Public/javascript/admision/main.js
@@ -1,38 +1,49 @@
- const down_angle = document.querySelectorAll('div.prsri-btn i.fas.fa-angle-up').forEach(a =>{
-  a.classList.add('angle-inactive')
-})  
-const info = document.querySelectorAll('.modal-info').forEach(a =>{
-  a.classList.add('angle-inactive')
-})  
+const down_angle = document.querySelectorAll("div.prsri-btn i.fas.fa-angle-up").forEach((a) => {
+  a.classList.add("angle-inactive");
+});
+const info = document.querySelectorAll(".modal-info").forEach((a) => {
+  a.classList.add("angle-inactive");
+});
 
-document.querySelector('.prsri').addEventListener("click", function(e){
-  e.preventDefault()
-  console.log(e.target.className)
+document.querySelector(".prsri").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(e.target.className);
   let btns = e.path[2];
-  let container = e.path[4]
-  if(e.target.className == 'fas fa-angle-down'){
-    btns.querySelector('.fas.fa-angle-up').classList.remove('angle-inactive')
-    btns.querySelector('.fas.fa-angle-down').classList.add('angle-inactive')
+  let container = e.path[4];
+  if (e.target.className == "fas fa-angle-down") {
+    btns.querySelector(".fas.fa-angle-up").classList.remove("angle-inactive");
+    btns.querySelector(".fas.fa-angle-down").classList.add("angle-inactive");
     /* div info */
-    container.querySelector('.modal-info').classList.remove('angle-inactive')
-    console.log(container)
-  }else{
-    btns.querySelector('.fas.fa-angle-down').classList.remove('angle-inactive')
-    btns.querySelector('.fas.fa-angle-up').classList.add('angle-inactive')
-      /* div info */
-    container.querySelector('.modal-info').classList.add('angle-inactive')
-    console.log(e)
+    container.querySelector(".modal-info").classList.remove("angle-inactive");
+    console.log(container);
+  } else {
+    btns.querySelector(".fas.fa-angle-down").classList.remove("angle-inactive");
+    btns.querySelector(".fas.fa-angle-up").classList.add("angle-inactive");
+    /* div info */
+    container.querySelector(".modal-info").classList.add("angle-inactive");
+    console.log(e);
   }
-})
+});
 
+const btnQuitForm = document.querySelector(".quit");
+btnQuitForm.addEventListener("click", function (e) {
+  document.querySelector(".modal-dialog").classList.add("is-hidden");
+  setTimeout(() => {
+    document.querySelector("#container-form").classList.add("is-hidden");
+  }, 1);
+});
 
-const btnQuitForm = document.querySelector('.quit')
-btnQuitForm.addEventListener("click", function (e){
-  
-  document.getElementById('form').classList.add('is-hidden')
-})
-
-const btn_info = document.querySelector('.icon-info')
-btn_info.addEventListener("click", function(e){
-  document.getElementById('form').classList.remove('is-hidden')
-})
+const btn_info = document.querySelector(".icon-info");
+btn_info.addEventListener("click", function (e) {
+  setTimeout(() => {
+    document.querySelector(".modal-dialog").classList.remove("is-hidden");
+  }, 1);
+  document.querySelector(".container-form").classList.remove("is-hidden"); 
+});
+const solicitud_info = document.querySelector(".btn.more-info-yellow");
+solicitud_info.addEventListener("click", function (e) {
+  setTimeout(() => {
+    document.querySelector(".container-form").classList.remove("is-hidden");
+  }, 1);
+  document.querySelector(".modal-dialog").classList.remove("is-hidden");
+});
---
+++ Public/javascript/master/main.js
@@ -0,0 +1,50 @@
+var child = document.querySelector('.router')
+var title = document.querySelector('title').textContent;
+child.innerHTML = title.slice(0, 35).concat("...");
+
+
+
+
+const nav_master = document.querySelector('.container-nav-master-grid')
+const hr_div = document.querySelectorAll('.container-nav-master-grid hr')
+const element_active = document.querySelectorAll('.container-nav-master-grid p')
+const element_desc = document.querySelectorAll('.container-nav-master')
+
+nav_master.addEventListener("click", function (e) {
+  //e.preventDefault();
+  e.stopPropagation();
+
+  let desc_select = e.toElement.classList[0].replace("select_active", "")
+
+  var hr_elem_select = e.target.nextElementSibling;
+  if (e.target.className.match("select_active") != 1) {
+    element_active.forEach((p) => {
+      p.classList.remove("select_active");
+    })
+    hr_div.forEach((hr) => {
+      e.target.classList.add('select_active')
+      hr.classList.remove("hr-yellow");
+    })
+
+
+    element_desc.forEach(text_div => {
+
+      if (text_div.className.includes(desc_select)) {
+        if (text_div.style.display == "none" || text_div.style.display.length < 1) {
+          text_div.style.display = "block"
+          text_div.classList.add("active")
+        } else {
+          console.log("quit")
+          text_div.style.display = "none"
+        }
+      } else if (text_div.classList.contains("active")) {
+        text_div.style.display = "none"
+      }
+    })
+    hr_elem_select.classList.add("hr-yellow")
+
+  }
+
+
+
+})
