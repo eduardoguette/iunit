0236f5ab4716a97d8b69bd13764e9428a1b57540
Eduardo Guette <eduardoguette@github.com>
2020-07-05 01:12:34 +0200

    programas

--- Public/javascript/programas/main.js
+++ Public/javascript/programas/main.js
@@ -10,8 +10,8 @@ container_selects.addEventListener("click", function (e) {
         elem_select.style.color = "white"
         if (elem_select.getAttribute("name") === "area" || elem_select.getAttribute("name") === "tipo") {
           elem_select.classList.add("seleccionado")
-          
-        }else{
+
+        } else {
           elem_select.classList.add("selected")
         }
 
@@ -20,7 +20,7 @@ container_selects.addEventListener("click", function (e) {
         elem_select.style.color = "#203354"
         if (elem_select.getAttribute("name") === "area" || elem_select.getAttribute("name") === "tipo") {
           elem_select.classList.remove("seleccionado")
-        }else{
+        } else {
           elem_select.classList.remove("selected")
         }
       }
@@ -29,25 +29,58 @@ container_selects.addEventListener("click", function (e) {
 })
 
 const selec_area = document.getElementById('area-filter')
-selec_area.addEventListener("change", function(e){
-  
+selec_area.addEventListener("change", function (e) {
+
   let option_select = selec_area.value
   console.log(option_select)
-  if(option_select.length > 0){
+  if (option_select.length > 0) {
     document.querySelector('a.one-select').innerHTML = ` ${option_select}`
-  }else{
+  } else {
     document.querySelector('a.one-select').innerHTML = ""
   }
 })
 
 const tipo = document.getElementById('tipo')
-tipo.addEventListener("change", function(e){
-  
+tipo.addEventListener("change", function (e) {
+
   let option_select = tipo.value
   console.log(option_select)
-  if(option_select.length > 0){
+  if (option_select.length > 0) {
     document.querySelector('a.two-select').innerHTML = ` / ${option_select}`
-  }else{
+  } else {
     document.querySelector('a.two-select').innerHTML = ""
   }
+})
+
+
+
+
+
+
+const header_desplegables = document.querySelectorAll('#desp-programas')
+header_desplegables.forEach(desplegable => {
+  desplegable.addEventListener("click", function (e) {
+    e.stopPropagation()
+    e.path.forEach(containter => {
+      if (containter.className === 'header-desp') {
+        if (containter.querySelector('p, .container-info > div').classList.contains('is-hidden')) {
+          containter.querySelector('p, .container-info > div').classList.remove('is-hidden')
+          containter.querySelector('i').classList.remove('fa-angle-down')
+          containter.querySelector('i').classList.add('fa-angle-up')
+          containter.querySelector('span').setAttribute("id", 'selected-desple')
+          //containter.querySelector('i').classList.remove('fas fa-angle-down')
+
+        } else {
+          containter.querySelector('p, .container-info > div').classList.add('is-hidden')
+          containter.querySelector('i').classList.add('fa-angle-down')
+          containter.querySelector('i').classList.remove('fa-angle-up')
+          containter.querySelector('span').classList.add('selected-desp')
+          containter.querySelector('span').removeAttribute("id", 'selected-desple')
+
+        }
+      }
+    })
+
+
+  })
 })
